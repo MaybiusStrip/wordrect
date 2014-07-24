a9640afdd3e6e4a9a6d2270665b77f0f869e46f2
Martin Wintz <martin.wintz@gmail.com>
2014-07-23 22:55:51 -0400

    Pseudocode for trieiterator.

--- trieIterator.c
+++ trieIterator.c
@@ -5,45 +5,74 @@
 TrieIterator *makeTrieIterator(char* solution[], TrieNode *root){
 
   TrieIterator *iter = malloc(sizeof(TrieIterator));
-  initPrefixes(iter, solution, root);
+  /*initPrefixes(iter, solution, root);*/
   /*goToFirstLeaf(iter);*/
+  set starting position
   return iter;
 }
 
-/*next (node) {*/
+void initialize
+  set the starting position
 
-  /*node = node.sibling*/
+next (node) {
 
-  /*if (node == null) {*/
-    /*if (node.parent == null) return null // it's over*/
-    /*node = node.parent*/
-    /*return next(node)*/
-  /*}*/
+  node = node.sibling
+  initNode()
 
-  /*if (node isnt leaf) {*/
-    /*node = node.firstChild*/
-    /*return next(node)*/
-  /*}*/
-
-  /*return node*/
-/*}*/
-
-
-void initPrefixes(TrieIterator *iter, char* solution[], TrieNode *root) {
-
-  // TODO: optimize this by passing it instead of calculating it
-  int lenSolutions = sizeof(solution[0]) / sizeof(solution[0][0]);
-  int numSolutions = sizeof(solution) / sizeof(solution[0]);
-
-  for (int i = 0; i < lenSolutions; i++) {
-
-    // Don't calculate these prefixes because square has same words in both directions
-    if (i < numSolutions) { break; }
-
-    iter->prefixes[i] = root;
-    for (int j = 0; j < numSolutions; j++) {
-      iter->prefixes[i] = iter->prefixes[i]->edges[solution[j][i]]->node;
-    }
+  if (node == null) {
+    if (node.parent == null) return null // it's over
+    node = node.parent
+    return next(node)
   }
 
+  if (node isnt leaf) {
+    node = node.firstChild
+    return next(node)
+  }
+
+  return node
 }
+
+initNode(node) {
+  node prefix = getPrefix
+  validChildren = prefix.children && node.children
+}
+
+getPrefix(solution, depth, wordTrie) {
+  for word in solution
+    wordTrie = wordTrie.children(word[depth])
+  return wordTrie
+}
+
+getValidChildren(prefix, node) {
+  prefix = copy(prefix)
+  charpointerwrite = prefix
+  charpointerread = prefix
+  while charpointerread
+    if node.children[*charpointerread]
+      charpointerwrite = *charpointerread
+      charpointerwrite++
+    charpointerread++
+  return prefix
+    
+
+
+
+/*void initPrefixes(TrieIterator *iter, char* solution[], TrieNode *root) {*/
+
+  /*// TODO: optimize this by passing it instead of calculating it*/
+  /*int lenSolutions = sizeof(solution[0]) / sizeof(solution[0][0]);*/
+  /*int numSolutions = sizeof(solution) / sizeof(solution[0]);*/
+
+  /*for (int i = 0; i < lenSolutions; i++) {*/
+
+    /*// Don't calculate these prefixes because square has same words in both directions*/
+    /*if (i < numSolutions) { break; }*/
+
+    /*iter->prefixes[i] = root;*/
+    /*for (int j = 0; j < numSolutions; j++) {*/
+      /*iter->prefixes[i] = iter->prefixes[i]->edges[solution[j][i]]->node;*/
+    /*}*/
+  /*}*/
+
+/*}*/
