14deb82ad5f4483eb4dbaf19445354854b7888be
Martin Wintz <martin.wintz@gmail.com>
2014-09-01 12:09:53 -0400

    Moved modules to lib folder.

--- js/lib/TrieNode.js
+++ js/lib/TrieNode.js
@@ -1,28 +1,30 @@
-function TrieNode(character, parent) {
+function TrieNode(character, parent, depth) {
   this.children = {};
   this.character = character;
   this.parent = parent;
+  this.depth = depth || 0;
 }
 
 TrieNode.prototype.addWord = function (word) {
 
+  if (!word.length) {
+    this.isTerminal = true;
+    return;
+  }
+
   var head = word[0];
   var tail = word.slice(1, word.length);
 
-  this.children[head] = this.children[firstChar] || new TrieNode(head, this);
+  this.children[head] = this.children[head] || new TrieNode(head, this, this.depth + 1);
 
-  if (tail.length) {
-    this.children[head].addWord(tail);
-  } else {
-    this.isTerminal = true;
-  }
+  this.children[head].addWord(tail);
 
 };
 
 TrieNode.prototype.getNodeForPrefix = function (prefix) {
 
-  var head = word[0];
-  var tail = word.slice(1, word.length);
+  var head = prefix[0];
+  var tail = prefix.slice(1, prefix.length);
   var child = this.children[head];
 
   if (tail.length === 0) { return child; }
---
+++ js/lib/WordSquareSolver.js
@@ -0,0 +1,37 @@
+function WordSquareSolver(trie, size) {
+  this.size = size;
+  this.trie = trie;
+}
+
+// Recursively try to fill the square, stopping at dead ends
+// Returns the most filled in partial square if it can't find
+// a complete solution.
+WordSquareSolver.prototype.solve = function (solution) {
+
+  if (!solution) {
+    solution = new WordSquareSolution(this.size, this.trie);
+  }
+
+  if (solution.isComplete()) {
+    return solution;
+  }
+
+  var iter = new SolutionIterator(solution);
+
+  var nextCandidateWord = iter.next();
+  while (nextCandidateWord) {
+
+    solution.push(nextCandidateWord);
+    solution = this.solve(solution);
+    if (solution.isComplete()) { return solution; }
+
+    solution.pop();
+    nextCandidateWord = iter.next();
+  }
+
+  return solution;
+
+};
+
+
+
